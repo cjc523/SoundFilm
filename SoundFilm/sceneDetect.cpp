5422fdf5eeb0d46236d9373f43cc90de684b5799
cjc523 <jc3413@imperial.ac.uk>
2016-05-22 07:48:43 +0100

    Testing the threshold of compare histogram algorithm.

--- SoundFilm/sceneDetect.cpp
+++ SoundFilm/sceneDetect.cpp
@@ -47,13 +47,15 @@ double cmpHist(Mat& last, Mat& current) {
     
     calcHist( &hsv_current, 1, channels, Mat(), hist_current, 2, histSize, ranges, true, false );
     normalize( hist_current, hist_current, 0, 1, NORM_MINMAX, -1, Mat() );
-    
+
     /// Apply the histogram comparison methods
     double diff = 0;
     //for( int i = 0; i < 4; i++ ) {
-        int compare_method = 0;
+        int compare_method = 2;
+    //cout << "hist last: " << hist_last << endl;
+    //cout << "hist next: " << hist_current << endl;
         diff = compareHist( hist_last, hist_current, compare_method );
-        //printf( " Method [%d] Difference : %f\n", i, diff );
+        printf( " Method [%d] Difference : %f\n", 2, diff );
     //}
     
     //printf( "Done \n" );
@@ -67,35 +69,66 @@ void detect(string fname) {
         throw "Error when opening the file";
     }
     cout << "open file successful" << endl;
-    double fps = round(vid.get(CV_CAP_PROP_FPS));
+    int fps = round(vid.get(CV_CAP_PROP_FPS));
     Mat current_frame;
     
     //double last_timestamp = 0;
     //double current_timeStamp;
-    //int frameCount = vid.get(CV_CAP_PROP_FRAME_COUNT);
+    int frameCount = vid.get(CV_CAP_PROP_FRAME_COUNT);
     namedWindow( "Display", WINDOW_AUTOSIZE );
     //if(vid.read(last_frame)) {
         cout << "starting scene detection" << endl;
-        
-        while (vid.read(current_frame)) {
-            Mat next_frame;
-            //if(floor(fmod(vid.get(CV_CAP_PROP_POS_FRAMES),fps)) == 0) {
-                vid.read(next_frame);
-            //cout << "detecting at " << vid.get(CV_CAP_PROP_POS_MSEC)/1000 << "s" << endl;
+    vid.read(current_frame);
+    Mat next_frame;
+    int j = 0;
+    int timestamp = 0;
+    double diff_last = 0;
+    for (int i = 1; i < frameCount-fps; i+=fps ) {
+        if(j%2 == 0) {
             //cout << "current frame is " << vid.get(CV_CAP_PROP_POS_FRAMES) << endl;
-            double diff = cmpHist(current_frame, next_frame);
-            if (diff <= 0.2) {
-                cout << "scene changed at " << vid.get(CV_CAP_PROP_POS_MSEC)/1000 << "s" << endl;
-                cout << "diffrence is " << diff << endl;
+            //if(floor(fmod(vid.get(CV_CAP_PROP_POS_FRAMES),fps/2)) == 0) {
+            for (int j = 0; j < fps; j++) {
+                vid.read(next_frame);
             }
-                /*last_frame = current_frame;
-                cout << "last = current "<< endl;
-                imshow( "Display", last_frame );
-                waitKey(1000);*/
-            //}
-        
-        
+            //imshow("Display", next_frame);
+            //waitKey(500);
+            //cout << "detecting at " << vid.get(CV_CAP_PROP_POS_MSEC)/1000 << "s" << endl;
+            cout << "next frame is " << vid.get(CV_CAP_PROP_POS_FRAMES) << endl;
+            double diff = cmpHist(current_frame, next_frame);
+            if (fabs((diff - diff_last)/diff_last) >= 0.8) {
+                if(vid.get(CV_CAP_PROP_POS_FRAMES) - timestamp > 8*fps) {
+                    cout << "scene changed at " << vid.get(CV_CAP_PROP_POS_MSEC)/1000 << "s" << endl;
+                    cout << "diffrence is " << diff << endl;
+                    timestamp = vid.get(CV_CAP_PROP_POS_FRAMES);
+                }
+            }
+            diff_last = diff;
+        } else {
+            //cout << "current frame is " << vid.get(CV_CAP_PROP_POS_FRAMES) << endl;
+            //if(floor(fmod(vid.get(CV_CAP_PROP_POS_FRAMES),fps/2)) == 0) {
+            for (int j = 0; j < fps; j++) {
+                vid.read(current_frame);
+            }
+            //imshow("Display", current_frame);
+            //waitKey(500);
+            //cout << "detecting at " << vid.get(CV_CAP_PROP_POS_MSEC)/1000 << "s" << endl;
+            cout << "next frame is " << vid.get(CV_CAP_PROP_POS_FRAMES) << endl;
+            double diff = cmpHist(next_frame, current_frame);
+            if (fabs((diff - diff_last)/diff_last) >= 0.8) {
+                if(vid.get(CV_CAP_PROP_POS_FRAMES) - timestamp > 8*fps) {
+                    cout << "scene changed at " << vid.get(CV_CAP_PROP_POS_MSEC)/1000 << "s" << endl;
+                    cout << "diffrence is " << diff << endl;
+                    timestamp = vid.get(CV_CAP_PROP_POS_FRAMES);
+                }
+            }
+            diff_last = diff;
         }
+        /*last_frame = current_frame;
+        cout << "last = current "<< endl;
+        imshow( "Display", last_frame );
+        waitKey(1000);*/
+        // }
+    }
         
         cout << "scene detect end" << endl;
     //} else {
